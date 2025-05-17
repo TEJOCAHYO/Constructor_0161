c50e50a55628641a14757078d0de50a26cfe5fd7
TEJOCAHYO <tejo.cahyo.ft24@mail.umy.ac.id>
2025-05-17 16:04:47 +0700

    menambahkan library

---
+++ pertemuan 10.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class buku
+{
+    string judul;
+
+public:
+    buku setjudul (sring judul);
+ {
+    this->judul = judul;
+    return *this // chain function
+ }
+string getJudul()
+{
+    return->this = judul;
+}
+
+}bukunya;
+
+int main()
+{
+    //bukunya.setjudul ("matematika");
+    //cout << bukunya.getjudul();
+     cout << bukunya.setjudul ("matematika").getjudul(); chaing function calls;
+     return 0; 
+}
+
+
